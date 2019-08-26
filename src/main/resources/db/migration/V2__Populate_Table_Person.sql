a5826c8ad39f9e96ef52a481fa82845d7aa05f09
anderkazuhiro <anderkazuhiro@hotmail.com>
2019-08-25 22:36:09 -0300

    implementação migration com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES
+(1,'sd js','Anderson','Male','kobayashi'),
+(2,'sd js','andreza','Female','cossa'),
+(6,'sd js','andreza','Female','costa'),
+(7,'sd js','andreza','Female','cardoso');
